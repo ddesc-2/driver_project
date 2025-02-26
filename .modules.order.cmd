d14a7cab22fa876f9654887a6be2ebd9a9f23301
wan wan chen <73027520+ddesc-2@users.noreply.github.com>
2025-02-27 00:04:20 +0800

    Delete .modules.order.cmd

--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/ddesc/linux-6.6.5/buildroot/gavin_module/modules.order := {   echo /home/ddesc/linux-6.6.5/buildroot/gavin_module/mymodule.o; :; } > /home/ddesc/linux-6.6.5/buildroot/gavin_module/modules.order
